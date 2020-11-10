483cbcf7068a77bce97437b235f4cdd03a54dd62
ariz-ahmad <ariz.ahmad@ufl.edu>
2020-11-09 23:21:17 -0500

    Parity
    Find parity of a number

---
+++ parity.py
@@ -0,0 +1,14 @@
+from test_framework import generic_test
+
+
+def parity(x: int) -> int:
+    # TODO - you fill in here.
+    count = 0
+    while(x > 0):
+        count ^= (x & 1)
+        x >>= 1
+    return count
+
+
+if __name__ == '__main__':
+    exit(generic_test.generic_test_main('parity.py', 'parity.tsv', parity))
